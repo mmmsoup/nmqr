bc6fcbcdde9bf500a34ec02c8a312fbd43fefaaa
Ben Jecock <benjecock@tutanota.com>
2022-09-08 17:48:12 +0100

    add scan support and redo some things

--- nmqr.sh
+++ nmqr.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 
-# takes a single argument: connection name
-function create_wifi_string() {
-	auth="WPA" # TODO: add support for other authentication types
-	ssid="$(nmcli -g 802-11-wireless.ssid connection show "$1")"
-	password="$(nmcli -s -g 802-11-wireless-security.psk connection show "$1")"
-	# TODO: implement these
-	#hidden=
-	#eap=
-	#anon_identity=
-	#identity=
-	#phase2=
-
-	wifi_str="WIFI:T:${auth};S:${ssid};P:${password};;"
-	
-	echo "$wifi_str"
-}
-
 if [ "$1" == "h" ] || [ "$1" == "help" ]; then
 	echo -e "'$0' - help"
 	echo -e "Usage: '$0' [SUBCOMMAND] [ARGS]"
@@ -29,24 +12,86 @@ if [ "$1" == "h" ] || [ "$1" == "help" ]; then
 elif [ "$1" == "l" ] || [ "$1" == "list" ]; then
 	nmcli -f name,type connection show | grep "wifi" | awk '{print $1}'
 elif [ "$1" == "r" ] || [ "$1" == "recv" ]; then
-	echo "TODO: implement this"
+	wifi_string=$(zbarcam -1 | grep -Po "(?<=^QR-Code:WIFI:).*(?=;)")
+	if [ -z "$wifi_string" ]; then
+		echo "FATAL: invalid code"
+		exit 1
+	else
+		oldifs="$IFS"
+		IFS=";"
+		for prop_string in $wifi_string; do
+			key="$(echo "$prop_string" | cut -d ":" -f 1)"
+			val="$(echo "$prop_string" | cut -d ":" -f 2)"
+			case "$key" in
+				"T") # authentication type
+					auth="$val"
+					;;
+				"S") # ssid
+					ssid="$val"
+					;;
+				"P") # password
+					password="$val"
+					;;
+				"H") # hidden
+					hidden="$val"
+					;;
+				"E") # eap method
+					eap="$val"
+					;;
+				"A") # anonymous identity
+					anon_identity="$val"
+					;;
+				"I") # identity
+					identity="$val"
+					;;
+				"PH2") # phase 2
+					phase2="$val"
+					;;
+				*) # default
+					echo "WARNING: unknown key '$key'"
+			esac
+		done
+		IFS="$oldifs"
+	fi
+	wifi_interface="$(nmcli -c no -g TYPE,DEVICE device status | grep -Po "(?<=^wifi:).*$")"
+	nmcli connection add \
+		type wifi \
+		con-name "$ssid" -- \
+		ifname "$wifi_interface" \
+		+802-11-wireless.ssid "$ssid" \
+		+wifi-sec.auth-alg open \
+		+wifi-sec.key-mgmt wpa-psk \
+		+wifi-sec.psk "$password"
 elif [ "$1" == "s" ] || [ "$1" == "share" ]; then
 	if [ -z "$2" ]; then
-		ssid="$(nmcli -g 802-11-wireless.ssid connection show "$(nmcli -g name connection show --active)")"
-		wifi_string=$(create_wifi_string "$(nmcli -g name connection show --active)")
+		# use current connection if no argument is given
+		conn_name="$(nmcli -f type,name connection show --active | grep "wifi" | awk '{print $2}')"
+		if [ -z "$conn_name" ]; then
+			echo "FATAL: no connection name given and currently not connected to a wireless network"
+			exit 1
+		fi
 	elif nmcli -g all connection show "$2" &> /dev/null; then
-		ssid="$2"
-		wifi_string=$(create_wifi_string "$2")
+		conn_name="$2"
 	else
-		echo "invalid connection name"
+		echo "FATAL: invalid connection name"
 		exit 1
 	fi
+	auth="WPA" # TODO: add support for other authentication types
+	ssid="$(nmcli -g 802-11-wireless.ssid connection show "$conn_name")"
+	password="$(nmcli -s -g 802-11-wireless-security.psk connection show "$conn_name")"
+	# TODO: implement these
+	#hidden=
+	#eap=
+	#anon_identity=
+	#identity=
+	#phase2=
+	wifi_string="WIFI:T:${auth};S:${ssid};P:${password};;"
 	qrencode -t ansiutf8 "$wifi_string"
 	echo "Scan to connect to $ssid"
 elif [ -z "$1" ]; then
-	echo "subcommand required: see '$0 help'"
+	echo "FATAL: subcommand required"
 	exit 1
 else
-	echo "unknown subcommand '$1'"
+	echo "FATAL: unknown subcommand '$1'"
 	exit 1
 fi
